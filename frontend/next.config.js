804d3241c3cd26049cec09495579354a79ca46f9
Joao Melo <joaohenrique1231500gmail.com>
2025-04-15 15:25:25 -0300

    feat: Adicionado configurações do Next.js para CORS e API

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,9 +4,42 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:3333/:path*",
+        destination: `${
+          process.env.NEXT_PUBLIC_API_URL ||
+          "https://nextjs-calendar-production.up.railway.app"
+        }/:path*`,
       },
     ];
+  },
+  async headers() {
+    return [
+      {
+        // Aplicar estas headers para todas as rotas
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Em produção, você deve especificar o domínio exato
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+  images: {
+    domains: ["nextjs-calendar-production.up.railway.app"],
   },
 };
 
